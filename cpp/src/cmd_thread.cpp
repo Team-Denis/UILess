f4bd52b0728f78dbde2c3770c9650101e7a70d62
Ahou Lucas <ahou.lucas@gmail.com>
2024-11-23 12:43:13 +0100

    wip

--- cpp/src/cmd_thread.cpp
+++ cpp/src/cmd_thread.cpp
@@ -6,15 +6,38 @@
 #include <vector>
 #include <mutex>
 
+#include "pipelineRunner.hpp"
+
 
 class ThreadSafeCmdProcessor {
     public:
         ThreadSafeCmdProcessor() : thread_running(false) {}
 
-        void push_task(std::vector<Command> &task) {
+        void start_thread() {
+            thread_running = true;
+            t(thread_func);
+        }
+
+        void stop_thread() {
+            running_mutex.lock();
+            thread_running = false;
+            running_mutex.unlock();
+
+            t.join();
+        }
+
+        void push_task(CommandPipeline &task, bool parallel) {
             tasks_mutex.lock();
             tasks.push(task);
             tasks_mutex.unlock();
+        }
+
+        bool isResultAvailable() {
+            results_mutex.lock();
+            size_t size = results.size();
+            results_mutex.unlock();
+
+            return size > 0;
         }
 
         Result pop_result() {
@@ -30,16 +53,35 @@ class ThreadSafeCmdProcessor {
         }
 
     private:
-        std::queue<std::vector<Command>> tasks;
+        std::thread t;
+        std::queue<CommandPipeline> tasks;
         std::queue<Result> results;
         std::mutex tasks_mutex;
         std::mutex results_mutex;
+        std::mutex running_mutex;
         bool thread_running;
 
-        std::vector<Command> pop_task() {
+        void thread_func() {
+
+            for (;;) {
+
+                running_mutex.lock();
+                if (thread_running == true) break;
+                running_mutex.unlock();
+
+                if (tasks.size() > 0) {
+                    std::vector<CommandPipeline> task = pop_task();
+                    Result res = runPipeline(task);
+                    push_result(res);
+                }
+
+            }
+        }
+
+        std::vector<CommandPipeline> pop_task() {
             tasks_mutex.lock();
 
-            std::vector<Command> out = tasks.front();
+            std::vector<CommandPipeline> out = tasks.front();
             tasks.pop();
 
             tasks_mutex.unlock();
