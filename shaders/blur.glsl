873dcc7937bb6dccd3f7f7837d54756f84aafb2e
Lucien Fiorini <lucienfiorini@gmail.com>
2024-11-23 13:57:43 +0100

    refactor gui

--- shaders/blur.glsl
+++ shaders/blur.glsl
@@ -6,8 +6,7 @@ in vec2 fragTexCoord;
 in vec4 fragColor;
 
 // Input uniform values
-uniform sampler2D texture0;
-uniform vec4 colDiffuse;
+uniform sampler2D texture;
 
 // Output fragment color
 out vec4 finalColor;
@@ -15,10 +14,19 @@ out vec4 finalColor;
 // NOTE: Add here your custom variables
 
 // NOTE: Render size values must be passed from code
-uniform vec2 resolution;
+vec2 resolution = vec2(870, 760);
 
-float offset[3] = float[](0.0, 1.3846153846, 3.2307692308);
-float weight[3] = float[](0.2270270270, 0.3162162162, 0.0702702703);
+vec4 blur9(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
+    vec4 color = vec4(0.0);
+    vec2 off1 = vec2(1.3846153846) * direction;
+    vec2 off2 = vec2(3.2307692308) * direction;
+    color += texture2D(image, uv) * 0.2270270270;
+    color += texture2D(image, uv + (off1 / resolution)) * 0.3162162162;
+    color += texture2D(image, uv - (off1 / resolution)) * 0.3162162162;
+    color += texture2D(image, uv + (off2 / resolution)) * 0.0702702703;
+    color += texture2D(image, uv - (off2 / resolution)) * 0.0702702703;
+    return color;
+}
 
 void main()
 {
@@ -31,6 +39,7 @@ void main()
         texelColor += texture(texture0, fragTexCoord - vec2(offset[i])/resolution.x, 0.0).rgb*weight[i];
     }
 
-    finalColor = vec4(texelColor, 1.0);
-    finalColor = vec4(1.0, 0, 0, 0.1);
+    finalColor = vec4(texelColor, 0.5);
+
+//    finalColor = vec4(1.0, 0, 0, 0.1);
 }
