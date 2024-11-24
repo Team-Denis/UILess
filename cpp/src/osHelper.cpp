3f2cca0baa9bf7b53b90b16eb6d61310b967d3e9
Ahou Lucas <ahou.lucas@gmail.com>
2024-11-24 12:42:16 +0100

    Added text dialog

--- cpp/src/osHelper.cpp
+++ cpp/src/osHelper.cpp
@@ -24,3 +24,23 @@ void open_file_dialog(std::string &result) {
 
     result = out;
 }
+
+void open_text_dialog(std::string &result) {
+    std::string out;
+    char buffer[128];
+
+    FILE* pipe = popen("zenity --entry --title=\"\" --text=\"\"", "r");
+    if (!pipe) {
+        throw std::runtime_error("popen() failed !");
+    }
+
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        out += buffer;
+    }
+
+    if (int returnCode = pclose(pipe); returnCode != 0) {
+        throw std::runtime_error("Command failed with return code: " + std::to_string(returnCode));
+    }
+
+    result = out;
+}
